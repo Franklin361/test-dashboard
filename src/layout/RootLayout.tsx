7a81344cbc070c8900f69354699605dfbc1a7e49
Franklin Martinez Lucas <frankomtzlucas361@gmail.com>
2024-05-31 12:29:46 -0600

    style: :lipstick: change media query

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -5,7 +5,7 @@ import {SideBar} from "../components/SideBar";
 import {SideBarMobile} from "../components/SideBarMobile";
 
 const RootLayout = () => {
-  const matches = useMediaQuery("(min-width: 1023px)");
+  const matches = useMediaQuery("(min-width: 1324px)");
 
   return (
     <div className="flex min-h-dvh grid-cols-[auto,1fr] justify-center gap-0 p-0 lg:grid lg:gap-10 lg:p-2">
